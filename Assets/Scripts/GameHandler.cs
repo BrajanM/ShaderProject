f21b993f3c2a94a5c4b0c2b855e8e3497c47aaef
BrajanM <brajan.mika@gmail.com>
2021-01-20 18:37:26 +0100

    FirstCommit

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+	public Terrain terrain;
+	public RenderTexture heightmap;
+	public Camera camera;
+	public GameObject brush;
+	public ComputeShader shader;
+
+    void Start()
+    {
+		heightmap = terrain.terrainData.heightmapTexture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetMouseButton(0))
+		{
+			var Ray = camera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(Ray, out hit))
+			{
+				Vector3 hitpoint = hit.point;
+				Debug.Log(hitpoint.ToString());
+				brush.transform.position = hit.point;
+
+			}
+		}    
+    }
+
+	public RenderTexture ApplyMaskToImage(RenderTexture render, Texture2D image, Texture2D mask)
+	{
+		int kernelIndex = shader.FindKernel("CalculateImage");
+		shader.SetTexture(kernelIndex, "Image", render);
+		shader.SetTexture(kernelIndex, "Image2D", image);
+		shader.SetTexture(kernelIndex, "ImageMask", mask);
+		shader.Dispatch(kernelIndex, 32, 32, 1);
+		return render;
+	}
+
+
+}
